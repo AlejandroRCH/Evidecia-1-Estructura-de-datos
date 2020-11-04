0ebab86f7d4e3b061dcff79ea7fec9a4d78b8d32
SammyDeAztlan <silver.palacios@gmail.com>
2020-11-04 11:38:10 -0600

    avance dado

--- source.py
+++ source.py
@@ -3,30 +3,33 @@
 #4.1 Aplicacion de listas como estructuras de datos nativas caso teorico
 
 #En este caso haremos el codigo que simule el lanzamiento de un dado con 6 lados
-
+import random
 separador = ("×" * 25)
-loop = True
-dado= list
-lados_dado= int(input("Dame el numero de caras que quieras para hacer el dado: \n"))
-for x in range(caras):
-    dado.append(x + 1)
-print(f"Estas son los lados del dado:"{dado})
+loop = 1
+dado= [1,2,3,4,5,6]
 print(separador)
 
-jugadores= int(input("Dime cuantos jugadores van a tirar el dado"))
+while loop == 1:
 
-
-while loop == True:
+    print(f"Estas son los lados del dado: ")
+    print(*dado)
+    jugadores= int(input("Dime cuantos jugadores van a tirar el dado "))
     print("Se procede a lanzar el dado creado")
     for turno in range(jugadores):
         print(f"Este es el resulado: {random.choice(dado)}")
 
-    continuar = input("Si desean tirar el dado de nuevo escribe\n Si\n No")
+    continuar = str(input("Si desean tirar el dado de nuevo escribe\n Si\n No\n"))
+    
     if continuar == "si" or "Si" or "SI":
         print("Nice")
+        print(separador)
+
     else:
         print("Vuelve pronto crack")
-        loop = False  
+        loop = 2
+        print(separador)
+
+
 
 
 
