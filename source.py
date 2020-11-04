97460cbbc75a818bb2841680afe14f273a2ba7c9
SammyDeAztlan <silver.palacios@gmail.com>
2020-11-03 20:05:28 -0600

    avance

---
+++ source.py
@@ -0,0 +1,14 @@
+#se empieza con la escritura del codigo del punto
+
+#4.1 Aplicacion de listas como estructuras de datos nativas caso teorico
+
+#En este caso necesitamos hacer uso de listas para hacer nuestra lista del mandado
+#para ello se usara el siguiente codigo
+
+lista_mandado= ["naranjas\n","leche\n","manzanas\n","huevos\n","1 kilo de tomates\n","1 kilo de cebolla\n"]
+print(', '.join(lista_mandado))
+
+#A continuacion se le dara al usuario la opcion de agregar algo mas a la lista
+nuevo_producto = input("Dime que deseas agregar al carrito")
+nuevo_producto= list
+lista_mandado.append(nuevo_producto)
