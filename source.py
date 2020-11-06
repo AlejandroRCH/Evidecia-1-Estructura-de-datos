d3a5ee8ff4dca9217a0c499229fefd0b9f6539c5
SammyDeAztlan <silver.palacios@gmail.com>
2020-11-05 18:05:46 -0600

    se realizan cambios al if 3

--- source.py
+++ source.py
@@ -76,16 +76,26 @@ print(separador +"\n")
 if eleccion == 3:
     import random
     loop = 1
-    playlist= []
-    rola= []
+    dado= [1,2,3,4,5,6,7,8,9,10,11,12]
     print(separador +"\n")
-    while loop:
-    for turno in range(10):
-        rola=input("Dime una rola para agregarla a la playlist: ")
-        playlist.append((rola)+"\n")
-    loop = 0
-    print("Esta es la playlist: ")
-    print(*playlist)
+    respuestas_validas=["si","SI","Si","sI"]
+    while loop == 1:
+        print(f"Estas son los lados del dado: ")
+        print(*dado)
+        jugadores= int(input("Dime cuantos jugadores van a tirar el dado "))
+        print("Se procede a lanzar el dado creado\n")
+        for turno in range(jugadores):
+            print(f"Este es el resulado: {random.choice(dado)}")
+            print(separador +"\n")
+        continuar = str(input("Si desean tirar el dado de nuevo escribe\n Si\n No\n"))
+        print(separador +"\n")
+        if continuar in respuestas_validas :
+            print("Nice")
+            print(separador +"\n")
+        else:
+            print("Vuelve pronto crack")
+            loop = 2
+            print(separador +"\n")
         
 if eleccion == 4:
     import random
